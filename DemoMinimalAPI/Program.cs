8f1d6740cf3ebc3c63a2831e434120aeed282e7a
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-19 12:32:47 -0300

    MapPut and MapDelete

--- DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/Program.cs
@@ -32,6 +32,7 @@ await context.Providers.FindAsync(id)
     is Provider provider 
         ? Results.Ok(provider)
         : Results.NotFound())
+
     .Produces<Provider>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound)
     .WithName("GetProviderById")
@@ -48,12 +49,52 @@ app.MapPost("/provider", async (
 
     return result > 0
      //? Results.Created($"/provider/{provider.Id}", provider)
-     ? Results.CreatedAtRoute("GetProviderById", new { id = provider.Id }, provider)
-     : Results.BadRequest("Houve um problema ao salvar");
+         ? Results.CreatedAtRoute("GetProviderById", new { id = provider.Id }, provider)
+         : Results.BadRequest("Houve um problema ao salvar");
+
 })  .ProducesValidationProblem()
     .Produces<Provider>(StatusCodes.Status201Created)
     .Produces(StatusCodes.Status400BadRequest)
     .WithName("PostProvider")
+    .WithTags("Provider");
+
+app.MapPut("/provider/{id}", async (Guid id, MinimalContextDb context, Provider provider) =>
+{
+    var providerDB = await context.Providers.FindAsync(id); 
+    if(providerDB == null) return Results.NotFound();
+
+    if (!MiniValidator.TryValidate(provider, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Providers.Update(provider);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao atualizar o Fornecedor");
+
+})  .ProducesValidationProblem()
+    .Produces<Provider>(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PutProvider")
+    .WithTags("Provider");
+
+app.MapDelete("/provider/{id}", async (Guid id, MinimalContextDb context) =>
+{
+    var provider = await context.Providers.FindAsync(id);
+    if (provider == null) return Results.NotFound();
+
+    context.Providers.Update(provider);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao remover o Fornecedor");
+
+})  .Produces<Provider>(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("DeleteProvider")
     .WithTags("Provider");
 
 app.Run();
