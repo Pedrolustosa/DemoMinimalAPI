24962b425dfeb44623f7bc160733d16a20127bbf
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-19 11:38:27 -0300

    First Entity and Configuration EF Core

--- DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/Program.cs
@@ -1,7 +1,12 @@
+using DemoMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
